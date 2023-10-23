15de9be497f3895d42331947c4b8ecc68c8ce576
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-10-23 16:01:22 -0500

    sales section bugfix responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -90,12 +90,12 @@ export default function Home() {
 
   function Sales() {
     return (
-      <div className="grid grid-cols-1  lg:grid-cols-4 gap-2 mx-auto max-w-[80%] my-16">
+      <div className="flex flex-col lg:grid lg:grid-cols-4 gap-2 mx-auto max-w-[80%] my-16">
         {
           sales.map((sale, index) => (
-            <div className='flex relative rounded-md h-[329px]' style={{ gridColumn: `span ${sale.size}` }}>
-              <Image src={sale.image} layout='fill' objectFit="cover" className='w-full rounded-md -z-10' alt={sale.image} />
-              <div className="flex flex-col justify-end h-full text-white gap-2 px-4 py-6">
+            <div className='flex relative rounded-md h-[400px] lg:h-[329px]' style={{ gridColumn: `span ${sale.size}` }}>
+              <Image src={sale.image} layout='fill' className='w-full rounded-md -z-10 object-fill' alt={sale.image} />
+              <div className="flex flex-col justify-end h-full text-white gap-2 px-6 py-6">
                 <div className="font-dmserif text-5xl">{sale.title}</div>
                 <div className="font-poppins text-xl">{sale.content}</div>
                 <div className="text-sm">
