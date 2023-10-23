24ee9ed5c10226e3ebf77fa55fe36f2d3695da59
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-10-22 21:22:09 -0500

    navbar from original project

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,106 +1,94 @@
 'use client';
-import React from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Input } from "@nextui-org/react";
-import { SearchIcon } from "./icons/search";
+import React, { useEffect, useState } from 'react'
+// import "./styles/Navbar.css"
+import { AppBar, Badge, Container, Grid, IconButton, Toolbar } from '@mui/material'
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+
+// import logo from "../assets/Group 79.svg"
+import { Favorite, ShoppingCart } from '@mui/icons-material';
+import Link from 'next/link';
+// import { useStatevalue } from '../StateProvider';
+// import SideBar from './utilities/SideBar';
+// import { actionTypes } from '../reducer';
 
 export default function CustomNavbar() {
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-    const menuItems = [
-        "Profile",
-        "Dashboard",
-        "Activity",
-        "Analytics",
-        "System",
-        "Deployments",
-        "My Settings",
-        "Team Settings",
-        "Help & Feedback",
-        "Log Out",
-    ];
+    // const [{basket},dispatch] = useStatevalue();  
 
+    // const openSearch = () =>{
+
+    //     dispatch({
+    //         type: actionTypes.OPEN_SEARCH,
+    //         open: true
+    //     })
+    // }
+
+
+
+    // console.log(basket);
     return (
-        <Navbar className="bg-[#033E8C] text-white" onMenuOpenChange={setIsMenuOpen}>
-            <NavbarContent>
-                <NavbarMenuToggle
-                    aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-                    className=""
-                />
-                <NavbarBrand>
-                    <img src="/icons/logo.png" alt="Logo" className="w-[75px] h-[37.5px]" />
-                </NavbarBrand>
-            </NavbarContent>
-
-            <NavbarContent className="hidden sm:flex gap-4" justify="start">
-                <Input
-                    classNames={{
-                        base: "max-w-full sm:max-w-[10rem] h-10",
-                        mainWrapper: "h-full",
-                        input: "text-small",
-                        inputWrapper: "h-full font-normal text-default-500 w-[300px]",
-                    }}
-                    placeholder="Type to search..."
-                    size="sm"
-                    startContent={<SearchIcon size={18} />}
-                    type="search"
-                />
-            </NavbarContent>
-            
-            <NavbarContent justify="end">
-                <NavbarItem className="hidden lg:flex">
-                    <Link href="#">Login</Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Button as={Link} color="primary" href="#" variant="flat">
-                        Sign Up
-                    </Button>
-                </NavbarItem>
-
-                <Dropdown placement="bottom-end">
-                    <DropdownTrigger>
-                        <Avatar
-                            isBordered
-                            as="button"
-                            className="transition-transform"
-                            color="secondary"
-                            name="Jason Hughes"
-                            size="sm"
-                            src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+        <AppBar position="static"
+            sx={{
+                backgroundColor: "#033E8C",
+                boxShadow: "none",
+                width: "100%"
+            }}
+        >
+            <Toolbar className='ToolBar'>
+                {/* <SideBar /> */}
+                <Link href="/">
+                    <IconButton>
+                        <img alt='' width={70} height={70} src="/icons/logo.png" />
+                    </IconButton>
+                </Link>
+                <Container
+                    sx={{ p: "auto", height: "35px", width: "811px" }}
+                >
+                    <Paper
+                        // onClick={openSearch}
+                        component="form"
+                        sx={{
+                            p: 'auto',
+                            display: 'flex',
+                            alignItems: 'center',
+                            width: "100%",
+                            height: "100%",
+                            margin: "auto",
+                            borderRadius: "300px"
+                        }}
+                    >
+                        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                            <SearchIcon />
+                        </IconButton>
+                        <InputBase
+                            sx={{ ml: 1, flex: 1 }}
+                            placeholder="¿Que producto estas buscando?"
+                            inputProps={{ 'aria-label': '¿Que producto estas buscando?' }}
                         />
-                    </DropdownTrigger>
-                    <DropdownMenu aria-label="Profile Actions" variant="flat">
-                        <DropdownItem key="profile" className="h-14 gap-2">
-                            <p className="font-semibold">Signed in as</p>
-                            <p className="font-semibold">zoey@example.com</p>
-                        </DropdownItem>
-                        <DropdownItem key="settings">My Settings</DropdownItem>
-                        <DropdownItem key="team_settings">Team Settings</DropdownItem>
-                        <DropdownItem key="analytics">Analytics</DropdownItem>
-                        <DropdownItem key="system">System</DropdownItem>
-                        <DropdownItem key="configurations">Configurations</DropdownItem>
-                        <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-                        <DropdownItem key="logout" color="danger">
-                            Log Out
-                        </DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
-            </NavbarContent>
-            <NavbarMenu>
-                {menuItems.map((item, index) => (
-                    <NavbarMenuItem key={`${item}-${index}`}>
-                        <Link
-                            color={
-                                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
-                            }
-                            className="w-full"
-                            href="#"
-                            size="lg"
-                        >
-                            {item}
-                        </Link>
-                    </NavbarMenuItem>
-                ))}
-            </NavbarMenu>
-        </Navbar>
-    );
+                    </Paper>
+                </Container>
+                <Link href="/login">
+                    <IconButton sx={{ color: "white" }}>
+                        <PersonIcon />
+                    </IconButton>
+                </Link>
+
+                <Link href="/favoritos">
+                    <IconButton sx={{ color: "white" }}>
+                        <Favorite />
+                    </IconButton>
+                </Link>
+                <Link href="/carrito">
+                    <IconButton sx={{ color: "white" }}>
+                        <Badge  badgeContent={0} color = "secondary">
+                            <ShoppingCart/> 
+                        </Badge>
+                    </IconButton>
+                </Link>
+            </Toolbar>
+        </AppBar>
+    )
 }
