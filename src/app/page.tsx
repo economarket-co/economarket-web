12dfc91dea71041c8d86e163abf563fa84b28c4a
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-10-23 14:51:41 -0500

    allies section and hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import { Grid, Paper, Typography } from '@mui/material'
-import { Button } from '@nextui-org/react'
-import Image from 'next/image'
-import Link from 'next/link'
+'use client';
+import SearchBar from '@/components/SearchBar';
+import { Button } from '@nextui-org/react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
 
@@ -53,26 +54,30 @@ export default function Home() {
       <Hero />
 
       <Sales />
+
+      <Allies />
+
+      <PreFooter />
     </main >
   )
 
   function Hero() {
     return (
-      <div className='flex flex-col flex-items justify-center items-center text-white relative'>
-        <img src="/images/login-bg.jpeg" className='max-h-[400px] w-full' />
+      <div className='flex flex-col flex-items justify-center items-center text-white relative max-h-[400px] overflow-hidden'>
+        <video src="/videos/home-bg.mp4" className='w-full' autoPlay />
 
-        <div className='absolute mx-auto flex flex-col gap-8 items-center z-50 max-w-[1000px]'>
+        <div className='absolute mx-auto flex flex-col gap-8 items-center z-40 max-w-[1000px] text-center'>
 
-          <h2 className='font-semibold text-6xl'>
+          <h2 className='font-normal text-6xl font-dmserif'>
             Lo que buscas, al mejor precio
           </h2>
 
-          <div className='font-medium'>
+          <div className='font-medium font-quicksand text-xl'>
             En Economarket puedes comparar los precios del mismo producto en diferentes supermercados. Añade productos a tu carrito y encuentra los precios perfectos para tu mercado.
           </div>
           <div >
             <Link href="/productos">
-              <Button className='bg-[#F28705] text-white text-lg rounded-md px-6 py-4 font-bold'>Empieza a mercar</Button>
+              <button className='bg-[#F28705] text-white text-lg rounded-md px-6 py-4 font-bold'>Empieza a mercar</button>
             </Link>
           </div>
         </div>
@@ -108,6 +113,64 @@ export default function Home() {
   }
 
   function Allies() {
-    
+    return (
+      <div className='flex flex-col flex-items justify-center items-center text-white relative' >
+        <img src="/images/allies-bg.png" className='max-h-[900px] w-full' />
+
+        <div className='absolute mx-auto py-5 flex flex-col gap-8 items-center z-50 max-w-[1000px]'>
+
+          <img src="/images/logo-white.png" />
+
+          <div className='text-[18px] font-medium text-center font-quicksand max-w-[700px]'>
+            Economarket es tu socio para una experiencia de compra ganadora. Colaboramos con reconocidos supermercados
+            de cadena como Éxito, Carulla, Jumbo y Olímpica para ofrecerte una herramienta que te permite encontrar
+            los mejores precios. Nos dedicamos a simplificar tu vida diaria, ahorrándote tiempo y dinero
+            con transparencia y seguridad.
+          </div>
+          <div className='flex flex:col lg:flex-row justify-between gap-10'>
+            <img src="/images/logo-exito.svg" />
+            <img src="/images/logo-jumbo.svg" />
+            <img src="/images/logo-carulla.svg" />
+            <img src="/images/logo-olimpica.svg" />
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  function PreFooter() {
+    return (
+      <div className='flex flex-col flex-items items-center text-white relative' >
+        <img src="/images/prefooter-bg.png" className='max-h-[900px] w-full' />
+
+        <div className='absolute mx-auto flex flex-col py-6 gap-8 items-center z-50 max-w-[1000px] text-white'>
+          <div className='text-center'>
+            <p className='font-quicksand text-3xl text-white'>Encontrar el mejor precio</p>
+            <p className='font-dmserif text-6xl font-semibold'>En solo tres pasos</p>
+          </div>
+
+          <div className='flex gap-6 justify-center'>
+            <div>
+              <p>Busca tu producto</p>
+            </div>
+
+            <div>
+              <p>Añadelo al carrito</p>
+            </div>
+
+            <div>
+              <p>Compara los precios</p>
+            </div>
+          </div>
+
+          <div>
+            <h3>{`Así que cuentanos`}</h3>
+
+            <SearchBar />
+          </div>
+        </div>
+      </div>
+
+    )
   }
 }
