20cf1dde1755fefe7658d277dae07c98b2dcbb60
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-10-26 11:31:34 -0500

    categories responsive fix

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import categories from '@/mock/categories.json';
 import CategoryCard from "@/components/cards/CategoryCard";
 import LinkButton from "@/components/buttons/LinkButton";
 import { Category } from '@prisma/client';
@@ -28,36 +27,40 @@ export default function Categories() {
             <div className='flex flex-col flex-items justify-center text-white relative py-16 overflow-hidden'>
                 <img src="/images/categories/categories-bg.png" className='absolute min-w-full min-h-full max-w-full -z-10' />
 
-                <div className="flex gap-4 items-center text-white divide-x-2 px-16">
-                    <h1 className="font-dmserif text-6xl max-w-[200px]">Siempre frescos</h1>
+                <div className="flex md:gap-4 items-center text-white divide-x-2 px-4 md:px-16">
+                    <h1 className="font-dmserif text-4xl md:text-6xl max-w-[200px]">Siempre frescos</h1>
 
-                    <p className="font-quicksand px-4 text-3xl max-w-[300px] opacity-85">50% en verduras seleccionadas</p>
+                    <p className="font-quicksand px-4 text-lg md:text-3xl max-w-[300px] opacity-85">50% en verduras seleccionadas</p>
                 </div>
             </div>
 
             <div className="flex flex-col px-10 gap-10 py-10">
                 <div className="flex flex-col gap-4">
-                    <h1 className="font-dmserif text-6xl">Categorías</h1>
+                    <h1 className="font-dmserif text-4xl md:text-6xl">Categorías</h1>
 
-                    <p className="font-quicksand text-xl">Los productos más buscados, organizados para ti </p>
+                    <p className="font-quicksand text-lg md:text-xl">Los productos más buscados, organizados para ti </p>
                 </div>
 
                 <div className="flex justify-center gap-7 flex-wrap">
                     {
                         categories.map((category, index) => (
-                            <CategoryCard 
-                                key={index}  
+                            <CategoryCard
+                                key={index}
                                 id={category.id}
                                 title={category.name}
                                 img={category.image}
-                                color={category.color}    
+                                color={category.color}
                             />
                         ))
                     }
                 </div>
             </div>
 
-            <div className='flex  justify-around items-center text-white relative py-16 overflow-hidden'>
+            <div className={`flex flex-col justify-around items-center text-center gap-6
+                lg:flex-row lg:text-start
+                text-white relative py-16 overflow-hidden`
+            }
+            >
                 <img src="/images/categories/categories-bg-2.png" className='absolute min-w-full min-h-full max-w-full -z-10' />
 
                 <div className="font-dmserif text-xl md:text-2xl lg:text-3xl">
