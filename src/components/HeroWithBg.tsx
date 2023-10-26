b8c9ba5d630e9fa16d86fadacce59603867303c8
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-10-26 11:33:28 -0500

    bugfix hero responsive

--- src/components/HeroWithBg.tsx
+++ src/components/HeroWithBg.tsx
@@ -7,8 +7,8 @@ export default function HeroWithBg(props: HeroWithBgProps) {
         <div className='flex flex-col flex-items justify-center text-white relative py-16 overflow-hidden'>
             <img src={props.BgImage} className='absolute min-w-full min-h-full max-w-full -z-10' />
 
-            <div className="flex gap-4 items-center text-white divide-x-2 px-16">
-                <h1 className="font-dmserif text-6xl">{props.title}</h1>
+            <div className="flex gap-4 items-center text-white divide-x-2 px-4 md:px-16">
+                <h1 className="font-dmserif text-4xl md:text-6xl">{props.title}</h1>
             </div>
         </div>
     )
