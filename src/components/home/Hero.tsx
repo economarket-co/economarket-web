dd3c2291cda7fc06e1ac040306f0cd9f5ae8bfe8
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-10-29 00:45:35 -0500

    fix hero

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -2,24 +2,41 @@ import { dmserif, quicksand } from "@/fonts";
 import Link from "next/link";
 
 export default function Hero() {
-    return (
-      <div className='flex flex-col flex-items justify-center items-center text-white relative min-h-screen py-16 overflow-hidden'>
-        <video src="/videos/home-bg.mp4" className='absolute min-w-full min-h-full max-w-none ' autoPlay />
+  return (
+    <div className={`flex flex-col flex-items justify-center items-center overflow-hidden 
+        text-white relative py-16  h-[400px]`}
+    >
+      <video
+        src="/videos/home-bg.mp4"
+        className='absolute min-w-full h-[600px] max-w-none object-cover'
+        autoPlay
+      />
 
-        <div className='mx-auto flex flex-col gap-8 items-center z-40 max-w-[300px] lg:max-w-[1000px] text-center'>
-          <h2 className={`font-normal text-6xl ${dmserif.className}`}>
-            Lo que buscas, al mejor precio
-          </h2>
+      <div
+        className="absolute min-w-full h-[600px] max-w-none"
+        style={{
+          backdropFilter: "blur(2px)",
+          backgroundBlendMode: "hard-light, normal, normal, normal",
+          background: "linear-gradient(0deg, rgba(0, 0, 0, 0.40) 0%, rgba(0, 0, 0, 0.40) 100%)"
+        }}
+      >
 
-          <div className={`font-medium ${quicksand.className} text-xl`}>
-            En Economarket puedes comparar los precios del mismo producto en diferentes supermercados. Añade productos a tu carrito y encuentra los precios perfectos para tu mercado.
-          </div>
-          <div >
-            <Link href="/products">
-              <button className={`bg-[#F28705] text-white text-lg rounded-md px-6 py-2 shadow-md ${dmserif.className} font-bold`}>Empieza a mercar</button>
-            </Link>
-          </div>
+      </div>
+
+      <div className='mx-auto flex flex-col gap-8 items-center z-40 max-w-[260px] md:max-w-[550px] lg:max-w-[1000px] text-center'>
+        <h2 className={`font-normal text-4xl lg:text-6xl ${dmserif.className}`}>
+          Lo que buscas, al mejor precio
+        </h2>
+
+        <div className={`font-medium ${quicksand.className} text-sm md:text-base lg:text-xl`}>
+          En Economarket puedes comparar los precios del mismo producto en diferentes supermercados. Añade productos a tu carrito y encuentra los precios perfectos para tu mercado.
+        </div>
+        <div >
+          <Link href="/products">
+            <button className={`bg-[#F28705] text-white text-lg rounded-md px-6 py-2 shadow-md ${dmserif.className} font-bold`}>Empieza a mercar</button>
+          </Link>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
