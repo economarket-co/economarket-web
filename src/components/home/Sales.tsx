88a98a191e76e578093bb0ca50ed3c9d06b61ee7
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-10-29 00:53:27 -0500

    fix sales

--- src/components/home/Sales.tsx
+++ src/components/home/Sales.tsx
@@ -5,15 +5,15 @@ import Link from 'next/link';
 
 export default function Sales() {
     return (
-        <div className="flex flex-col lg:grid lg:grid-cols-4 gap-2 mx-auto max-w-[80%] my-16">
+        <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-4 gap-2 mx-auto max-w-[80%] my-16">
             {
                 sales.map((sale, index) => (
                     <div
                         key={index}
-                        className='flex relative rounded-md h-[400px] lg:h-[329px]'
+                        className='flex relative rounded-md h-[400px] md:h-[246px] lg:h-[329px]' 
                         style={{ gridColumn: `span ${sale.size}` }}
                     >
-                        <Image src={sale.image} layout='fill' className='w-full rounded-md -z-10 object-fill' alt={sale.image} />
+                        <Image src={sale.image} layout='fill' className='w-full rounded-md -z-10 object-cover' alt={sale.image} />
                         <div className="flex flex-col justify-end h-full text-white gap-2 px-6 py-6">
                             <div className={`${dmserif.className} text-5xl`}>{sale.title}</div>
                             <div className={`${poppins.className} text-xl`}>{sale.content}</div>
