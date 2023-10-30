2420c148baa017832900ee29ff18dd1294252d0b
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-10-30 16:04:06 -0500

    comparador de precios responsive

--- src/components/cards/SuperMarketCard.tsx
+++ src/components/cards/SuperMarketCard.tsx
@@ -12,14 +12,14 @@ type SuperMarketCardProps = {
 }
 export function SuperMarketCard(props: SuperMarketCardProps) {
     return (
-        <Card className=" lg:w-[300px] xl:w-[320px]">
+        <Card className="w-[160px] lg:w-[230px] xl:w-[320px]">
             <CardBody className="p-0">
-                <img src={props.img} className="h-[110px] object-fill" alt="olimpica" />
+                <img src={props.img} className="h-[92xp] md:h-[50px] lg:h-[110px] object-fill" alt="olimpica" />
             </CardBody>
             <CardFooter className="flex flex-col gap-2">
-                <p className={`${quicksand.className} text-3xl font-medium`}>{props.price ? formatCurrency(props.price) : "No disponible"}</p>
-                <p className={`${quicksand.className} text-lg text-[#646464]`}>{props.quantityPerUnit} {props.unit}</p>
-                <p className={`${quicksand.className} text-lg text-[#646464]`}>
+                <p className={`${quicksand.className} text-[14pxs] lg:text-[18px] xl:text-3xl font-medium`}>{props.price ? formatCurrency(props.price) : "No disponible"}</p>
+                <p className={`${quicksand.className} text-[10px] lg:text-[14px] xl:text-xl text-[#646464]`}>{props.quantityPerUnit} {props.unit}</p>
+                <p className={`${quicksand.className} text-[10px] lg:text-[14px] xl:text-xl text-[#646464]`}>
                     {
                         !props.unitPrice ?
                             "No disponible"
