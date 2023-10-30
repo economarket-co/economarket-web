0b588afbde5003654722ecbe6cea5ef9249d4ea8
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-10-30 14:33:06 -0500

    admin dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,10 @@
 export default function Dashboard() {
     return (
-        <div>
-            sssss
-        </div>
+        <iframe 
+            src="https://lookerstudio.google.com/embed/reporting/2bccb9c8-5a14-40ed-bf88-138fa9f1b6a6/page/UGpgD"
+            className="w-full h-full"    
+        >
+            
+        </iframe>
     )
 }
