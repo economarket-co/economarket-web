0b5158c43f7280a22e5cedc6c50d3624de077b2d
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-11-02 07:49:19 -0500

    show preview on product creation

--- src/app/admin/products/add/page.tsx
+++ src/app/admin/products/add/page.tsx
@@ -1,8 +1,13 @@
 "use client";
+import AddToCartButton from "@/components/buttons/AddToCartButton";
+import ProductCard from "@/components/cards/ProductsCard";
 import Form from "@/components/forms/Form";
+import { quicksand } from "@/fonts";
 import { uploadFilesFromClient } from "@/utils/uploadFilesFromClient";
+import { Card, CardBody, CardFooter } from "@nextui-org/react";
 import { Category } from "@prisma/client";
 import axios from "axios";
+import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
@@ -28,16 +33,20 @@ export default function EditCategoryPage({ params }: any) {
         { label: "Sub Categoria", placeholder: "Selecciona una sub categoría", isRequired: true, type: "select", value: categories, options: subCategoriesList, onChange: setCategories },
         { label: "Unidad de medida", placeholder: "Ingresa la unidad de medida ej: Gr, Ml etc...", isRequired: true, type: "text", value: unit, onChange: setUnit },
         { label: "Cantidad por unidad", placeholder: "Ingresa la cantidad por unidad", isRequired: true, type: "number", value: quantityPerUnit, onChange: setQuantityPerUnit },
-        { label: "Link Carulla", placeholder: "Ingresa el link de Carulla", isRequired: true, type: "text", value: linkCarulla, onChange: setLinkCarulla},
-        { label: "Link Exito", placeholder: "Ingresa el link de Exito", isRequired: true, type: "text", value: linkExito, onChange: setLinkExito},
-        { label: "Link Jumbo", placeholder: "Ingresa el link de Jumbo", isRequired: true, type: "text", value: linkJumbo, onChange: setLinkJumbo},
-        { label: "Link Olimpica", placeholder: "Ingresa el link de Olimpica", isRequired: true, type: "text", value: linkOlimpica, onChange: setLinkOlimpica},
+        { label: "Link Carulla", placeholder: "Ingresa el link de Carulla", isRequired: true, type: "text", value: linkCarulla, onChange: setLinkCarulla },
+        { label: "Link Exito", placeholder: "Ingresa el link de Exito", isRequired: true, type: "text", value: linkExito, onChange: setLinkExito },
+        { label: "Link Jumbo", placeholder: "Ingresa el link de Jumbo", isRequired: true, type: "text", value: linkJumbo, onChange: setLinkJumbo },
+        { label: "Link Olimpica", placeholder: "Ingresa el link de Olimpica", isRequired: true, type: "text", value: linkOlimpica, onChange: setLinkOlimpica },
         { label: "Imagen", placeholder: "Selecciona una imagen", isRequired: true, type: "file", value: image as File, onChange: setImage }
     ]
 
     useEffect(() => {
         fetchData();
     }, []);
+
+    useEffect(() => {
+        console.log(image)
+    }, [image])
 
 
     async function fetchData() {
@@ -89,13 +98,37 @@ export default function EditCategoryPage({ params }: any) {
     }
 
     return (
-        <main>
+        <main className="flex items-center gap-8 ">
             <Form
                 title="Agregar Producto"
                 fields={fields}
                 handleSubmit={handleSubmit}
                 loading={loading}
             />
+
+            <Card className='w-[150px] lg:w-[230px] border shrink-0' style={{ boxShadow: "0px 2px 12px 0px rgba(0, 0, 0, 0.25)" }}>
+                <CardBody className='relative p-0 h-[112px] lg:h-[224px] border-b'>
+                    <img className='h-full object-fill' src={`${image ? URL.createObjectURL(image) : ''}`} alt={name} />
+                </CardBody>
+                <CardFooter className='flex flex-col gap-2 px-4 items-start overflow-x-hidden'>
+                    {/* <Tooltip content={props.product.name} placement="bottom"> */}
+                    <p className={`${quicksand.className} text-[9px] lg:text-xs text-[#343434] overflow-clip`}>{name}</p>
+                    {/* </Tooltip> */}
+                    <p className={`${quicksand.className} text-xs text-[#646464] `}>{quantityPerUnit} {unit}</p>
+                    <div className="flex gap-2">
+                        <img src='/icons/price-tag.svg' />
+                        <p className={`${quicksand.className} text-[9px] lg:text-xs text-[#646464]`}>
+                            Carrulla, Exito, Olimpica, Jumbo
+                        </p>
+                    </div>
+                    
+                    <AddToCartButton
+                        //@ts-ignore
+                        product={null}
+                        quantity={1}
+                    />
+                </CardFooter>
+            </Card>
         </main>
     )
 }
