c18163484ff0db221990eba72869cab3e549c8ba
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-11-02 17:33:19 -0500

    add to cart animation

--- src/components/buttons/AddToCartButton.tsx
+++ src/components/buttons/AddToCartButton.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { Button } from "@nextui-org/react";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CartContext } from "@/Context/CartContext";
 import { Product, ProductPrice } from "@prisma/client";
 import { CreateCardItem } from "@/odt/CardItem/createCardItem.odt";
+import { motion } from "framer-motion";
 
 type AddToCartButtonProps = {
     priceId?: number,
@@ -12,7 +13,8 @@ type AddToCartButtonProps = {
 }
 
 export default function AddToCartButton(props: AddToCartButtonProps) {
-    const { addToCart } = useContext(CartContext);
+    const { addToCart, cartItems } = useContext(CartContext);
+    const [item, setItem] = useState<any>(null);
 
     const product: CreateCardItem = {
         product: props.product,
@@ -20,14 +22,48 @@ export default function AddToCartButton(props: AddToCartButtonProps) {
         priceId: props.priceId
     }
 
+    useEffect(() => {
+        const item = cartItems.find(item => item.product.id === props.product.id);
+        setItem(item);
+    }, [cartItems])
+
     return (
-        <button 
-            onClick={e => addToCart(product, props.quantity)}
-            className={`bg-[#01CC5E] text-white w-full rounded-md flex gap-2 items-center justify-center py-2 px-4
+        <button
+            onClick={e => item ? null : addToCart(product, props.quantity)}
+            className={`${item ? "bg-white" : "bg-[#01CC5E]"} hover:bg-white hover:text-[#01CC5E] text-white w-full rounded-md flex gap-2 items-center justify-center py-2 px-4
                 text-[9px] lg:text-[14px] xl:text-xl font-medium
             `}
-        >
-            <span>Agregar</span> <img src="/icons/add-to-shopping-cart.svg" className="w-3 h-3 lg:w-4 lg:h-4" />
+            style={{ boxShadow: "0px 2px 12px 0px rgba(0, 0, 0, 0.25)" }}
+        >   {
+                item ?
+                    <div className="flex justify-between items-center w-full">
+                        <motion.img
+                            src="/icons/add-to-cart-green.svg"
+                            className="w-6 h-6"
+                            animate={{ translateX: [100, 0] }}
+                            transition={{ duration: 0.6 }}
+                        />
+
+                        <motion.div
+                            className="flex gap-4 text-[#01CC5E] text-[20px] "
+                            animate={{ opacity: [0, 1] }}
+                            transition={{ duration: 1.5 }}
+                        >
+                            <button onClick={e => addToCart(item, -1)}>-</button>
+                            <span>{item.quantity}</span>
+                            <button onClick={e => addToCart(item, 1)}>+</button>
+                        </motion.div>
+                        <div>
+
+                        </div>
+                    </div>
+                    :
+                    <>
+                        <span className="text-base">Agregar</span>
+                        <img src="/icons/add-to-shopping-cart.svg" className="hover:text-[#01CC5E]" />
+                    </>
+            }
+
         </button>
     )
 }
