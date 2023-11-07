cea2ff6e03cba018e925f402a7771bd7ad8956dc
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-11-06 21:58:07 -0500

    green cart on product card hover

--- src/components/buttons/AddToCartButton.tsx
+++ src/components/buttons/AddToCartButton.tsx
@@ -16,6 +16,7 @@ type AddToCartButtonProps = {
 export default function AddToCartButton(props: AddToCartButtonProps) {
     const { addToCart, cartItems } = useContext(CartContext);
     const [item, setItem] = useState<any>(null);
+    const [hover, setHover] = useState(false);
 
     const product: CreateCardItem = {
         product: props.product,
@@ -30,12 +31,20 @@ export default function AddToCartButton(props: AddToCartButtonProps) {
     }, [cartItems])
 
     return (
-        <button
+        <motion.button
+            onMouseEnter={e => setHover(true)}
+            onMouseLeave={e => setHover(false)}
             onClick={e => item ? null : addToCart(product, props.quantity)}
-            className={`${item ? "bg-white" : "bg-[#01CC5E]"} hover:bg-white hover:text-[#01CC5E] text-white w-full rounded-md flex gap-2 items-center justify-center py-2 px-4
+            className={`w-full rounded-md flex gap-2 items-center justify-center py-2 px-4
                 text-[9px] lg:text-[14px] xl:text-xl font-medium
             `}
-            style={{ boxShadow: "0px 2px 12px 0px rgba(0, 0, 0, 0.25)" }}
+            style={{ boxShadow: "0px 2px 12px 0px rgba(0, 0, 0, 0.25)", backgroundColor: item ? "white" : "#01CC5E", color: item ? "#01CC5E" : "white" }}
+            transition={{ duration: 0.3 }}
+            whileHover={{
+                backgroundColor: "white",
+                color: "#01CC5E",
+                scale: 1.05,
+            }}
         >   {
                 item ?
                     <div className="flex justify-between items-center w-full">
@@ -49,7 +58,7 @@ export default function AddToCartButton(props: AddToCartButtonProps) {
                         <motion.div
                             className="flex gap-4 text-[#01CC5E] text-[20px] "
                             animate={{ opacity: [0, 1] }}
-                            transition={{ duration: 1.5 }}
+                            transition={{ duration: 2 }}
                         >
                             <button onClick={e => addToCart(item, -1)}>-</button>
                             <span>{item.quantity}</span>
@@ -62,10 +71,25 @@ export default function AddToCartButton(props: AddToCartButtonProps) {
                     :
                     <>
                         <span className="text-base">Agregar</span>
-                        <img src="/icons/add-to-shopping-cart.svg" className="hover:text-[#01CC5E]" />
+                        {
+                            hover ?
+                                <motion.img
+                                    src="/icons/add-to-cart-green.svg"
+                                    className="w-6 h-6"
+                                    animate={{ translateX: [100, 0] }}
+                                    transition={{ duration: 0.6 }}
+                                /> :
+                                <motion.img
+                                    src="/icons/add-to-shopping-cart.svg"
+                                    className="w-6 h-6"
+                                    animate={{ translateX: [100, 0] }}
+                                    transition={{ duration: 0.6 }}
+                                />
+                        }
+                        {/*<img src="/icons/add-to-shopping-cart.svg" className="hover:text-[#01CC5E]" /> */}
                     </>
             }
 
-        </button>
+        </motion.button>
     )
 }
