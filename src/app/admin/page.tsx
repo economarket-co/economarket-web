08f22b3a8005a6d34e9d30589ddd6ee874c870fe
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-11-17 12:16:37 -0500

    change users dashboard url

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
     return (
         <iframe 
-            src="https://lookerstudio.google.com/embed/reporting/2bccb9c8-5a14-40ed-bf88-138fa9f1b6a6/page/UGpgD"
+            src="https://lookerstudio.google.com/embed/reporting/3577d5da-ee96-476d-9035-af5914ae1697/page/UGpgD"
             className="w-full h-full"    
         >
             
