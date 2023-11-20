6b9380ca66704466844bb18b90e452c6a7b40d0f
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-11-20 09:57:39 -0500

    Update price range display in ProductsFilter component

--- src/components/filters/ProductsFilter.tsx
+++ src/components/filters/ProductsFilter.tsx
@@ -106,7 +106,7 @@ export default function ProductsFilter(props: ProductsFilterProps) {
                             onChange={handlePriceRange}
                         />
 
-                        <p>{formatCurrency(0)} - {formatCurrency(props.maxPrice)}</p>
+                        <p>{formatCurrency(0)} - {formatCurrency(props.priceRange)}</p>
                     </div>
 
                     <div className="flex flex-col gap-4 py-7">
